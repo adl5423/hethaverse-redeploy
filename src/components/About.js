af6e063cf75bf0ce821b9ea72b99b683a6f6006c
Aisen Lopez <aisenlopez@hotmail.com>
2022-03-17 21:53:14 -0400

    about us hero

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import Aos from 'aos';
+import Consulting from './images/consulting.jpeg'
 import 'aos/dist/aos.css';
 import './About.scss';
 
@@ -10,8 +11,13 @@ function About() {
 
     return (
         <>
-            <div className="page-container">
-                <h1> It's working </h1>
+            <div className="about-us-container">
+                <h1 className="our-mission">
+                    We Believe Health and Wealth is on the top of the pyramid
+                </h1>
+                <div className="hero">
+                    <img src={Consulting} alt="consulting" className="hero-background" />
+                </div>
             </div>
         </>
     )
